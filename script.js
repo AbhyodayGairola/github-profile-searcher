7232db1c5ce2eb2e4dec0d56eded31542ef627f6
AbhyodayGairola <abhyodaygairola1@gmail.com>
2026-08-29 20:36:59 +0530

    Fix API URL for deployment

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function searchUser() {
     try {
 
         const response = await fetch(
-            `http://127.0.0.1:8000/api/github/${encodeURIComponent(username)}`
+            `/api/github/${encodeURIComponent(username)}`
         );
 
         if (!response.ok) {
